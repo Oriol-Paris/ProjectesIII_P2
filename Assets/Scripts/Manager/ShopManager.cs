075c1541f53408b4bc37bfac8c5cbfbd1a405aba
ORIOL PARÍS FONT <oriol.paris@enti.cat>
2024-11-13 12:03:28 +0100

    Shop Started

--- Assets/Scripts/Manager/ShopManager.cs
+++ Assets/Scripts/Manager/ShopManager.cs
@@ -1,23 +1,51 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
+using NUnit.Framework.Internal;
+using UnityEngine.UI;
+
 public class ShopManager : MonoBehaviour
 {
+    public enum ShopItems { SHOTGUN, HEAL }
+
     public TextMeshProUGUI rerollText;
+    [SerializeField] public List<TextMeshProUGUI> itemTexts;
+    [SerializeField] public List<TextMeshProUGUI> pricePool;
+    public List<ShopItems> itemPool;
     public int rerollPrice;
+
+    public void Start()
+    {
+        itemPool = new List<ShopItems>();
+
+        itemPool.Add(ShopItems.SHOTGUN);
+        itemPool.Add(ShopItems.HEAL);
+
+        itemTexts[0].text = "Shotgun";
+        itemTexts[1].text = "Heal";
+
+        pricePool[0].text = Random.Range(50, 100)+"";
+        pricePool[1].text = Random.Range(50, 100)+"";
+    }
+
     public void Reroll()
     {
         rerollPrice++;
         rerollText.text = rerollPrice+"";
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+
+        for(int i = 0; i < pricePool.Count; i++)
+        {
+            pricePool[i].text = Random.Range(50, 100).ToString();
+        }
+
+        /*foreach (Button button in FindObjectsOfType<Button>())
+        {
+            button.GetComponent<GameObject>().SetActive(true);
+        }*/
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BuyItem()
     {
-        
+        //Do something
     }
 }
