74d32f7420c5ab85bbb4e394eba1b59719a6fd6e
JAN PLANES MAZCUÑAN <jan.planes@enti.cat>
2024-11-13 20:16:02 +0100

    EXP visible, feedback when buying

--- Assets/Scripts/Manager/ShopManager.cs
+++ Assets/Scripts/Manager/ShopManager.cs
@@ -11,19 +11,26 @@ public class ShopManager : MonoBehaviour
     [SerializeField] public List<int> pricePool;
     [SerializeField] public List<TextMeshProUGUI> priceTexts;
     [SerializeField] public PlayerBase player;
+    [SerializeField] public TextMeshProUGUI boughtItem;
+    [SerializeField] public TextMeshProUGUI currentXP;
 
     public int rerollPrice;
 
     public void Start()
     {
+        boughtItem.enabled = false;
         itemTexts[0].text = "Shotgun";
         itemTexts[1].text = "Heal";
         priceTexts[0].text = pricePool[0].ToString();
         priceTexts[1].text = pricePool[1].ToString();
         pricePool[0] = Random.Range(5, 10);
         pricePool[1] = Random.Range(5, 10);
+        currentXP.text = player.playerData.exp + "";
     }
-
+    public void Update()
+    {
+        currentXP.text = player.playerData.exp + "";
+    }
     public void Reroll()
     {
         if(player.playerData.exp>=rerollPrice) { 
@@ -54,6 +61,13 @@ public class ShopManager : MonoBehaviour
                         player.playerData.exp -= pricePool[i];
                         player.playerData.availableActions.Add(new PlayerData.ActionData
                         (PlayerBase.ActionType.ACTIVE, PlayerBase.ActionEnum.SHOOT, KeyCode.Alpha3, player.playerData.shotgun));
+                        boughtItem.enabled = true;
+                        boughtItem.text = "Just bought: Shotgun";
+                    }
+                    else
+                    {
+                        boughtItem.enabled = true;
+                        boughtItem.text = "Not enough experience";
                     }
                     
                 }
@@ -66,7 +80,14 @@ public class ShopManager : MonoBehaviour
                     {
                         player.playerData.exp -= pricePool[i];
                         player.AddNewAction(new PlayerBase.Action(PlayerBase.ActionType.PASSIVE, PlayerBase.ActionEnum.HEAL, KeyCode.Alpha4));
+                        boughtItem.enabled = true;
+                        boughtItem.text = "Just bought: Heal";
 
+                    }
+                    else
+                    {
+                        boughtItem.enabled = true;
+                        boughtItem.text = "Not enough experience";
                     }
                 }
                 break;
