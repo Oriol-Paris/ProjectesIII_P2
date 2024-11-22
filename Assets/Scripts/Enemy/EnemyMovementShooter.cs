a3a9c83ee92b321b929a1f2b4231951a358da26a
JanPlanes <jan.planes@enti.cat>
2024-11-22 11:47:04 +0100

    rebalance

--- Assets/Scripts/Enemy/EnemyMovementShooter.cs
+++ Assets/Scripts/Enemy/EnemyMovementShooter.cs
@@ -41,7 +41,7 @@ public class EnemyMovementShooter : MonoBehaviour
             bool currentIsMoving = closestPlayer.GetIsMoving();
 
             // Only take action on a new turn (when GetIsMoving toggles from false to true)
-            if (closestPlayer.t <= 1 && currentIsMoving)
+            if (!currentIsMoving)
             {
                 if (!haveChosenAnAction)
                 {
